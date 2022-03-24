71e9e2959c02df0f1e4cc828959b78b5629220e5
Andrew Goetz <agoetz4407@gmail.com>
2022-03-24 16:01:30 -0500

    added final styling and animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,7 +160,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -196,6 +196,16 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: 'pointer',
   helper: 'clone',
+  activate: function(){
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
+  },
+  deactivate: function(){
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
+  },
+  over: function(event){$(event.target).addClass('dropover-active')},
+  out: function(event){$(event.target).removeClass('dropover-active')},
   update: function(event) {
     //array to store task data
     var tempArr = [];
@@ -229,9 +239,12 @@ $(".card .list-group").sortable({
 $('#trash').droppable({
   accept: '.card .list-group-item',
   tolerance: 'touch',
+  over: function(){$('.bottom-trash').addClass('bottom-trash-active')},
   drop: function(event, ui) {
     ui.draggable.remove();
-  }
+    $('.bottom-trash').removeClass('bottom-trash-active');
+  },
+  out: function(){$('.bottom-trash').removeClass('bottom-trash-active')}
 });
 
 var auditTask = function(taskEl) {
@@ -258,6 +271,13 @@ var auditTask = function(taskEl) {
 $('#modalDueDate').datepicker({
   minDate: 1
 });
+
+//audit tasks every 30 mins to check for past due and coming up tasks
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
